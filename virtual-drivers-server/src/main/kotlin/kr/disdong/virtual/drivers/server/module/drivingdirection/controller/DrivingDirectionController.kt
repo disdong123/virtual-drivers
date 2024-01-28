f372de2bf70a265b5a65aae9b32aa72731975e8a
disdong123 <gksrlfw@gmail.com>
2024-01-28 23:13:36 +0900

    http test 추가

--- virtual-drivers-server/src/main/kotlin/kr/disdong/virtual/drivers/server/module/drivingdirection/controller/DrivingDirectionController.kt
+++ virtual-drivers-server/src/main/kotlin/kr/disdong/virtual/drivers/server/module/drivingdirection/controller/DrivingDirectionController.kt
@@ -15,8 +15,6 @@ class DrivingDirectionController(
     private val drivingDirectionService: DrivingDirectionService,
 ) {
 
-    // http://localhost:8080/api/driving-direction?startLatitude=127.001&startLongitude=37.001&goalLatitude=127.001&goalLongitude=37.001
-    // http://localhost:8080/api/driving-direction?startLongitude=126.9918&startLatitude=37.5519&goalLongitude=126.7052&goalLatitude=37.4563
     @GetMapping
     fun getDrivingDirection(
         @ModelAttribute request: GetDrivingDirectionRequest,
