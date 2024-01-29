8d7da5c6d4755ce35a37336a674ad6478b8a0016
disdong123 <gksrlfw@gmail.com>
2024-01-29 22:33:43 +0900

    naver map 추가

--- virtual-drivers-server/build.gradle.kts
+++ virtual-drivers-server/build.gradle.kts
@@ -4,6 +4,8 @@ dependencies {
     implementation(project(":virtual-drivers-common"))
     implementation(project(":virtual-drivers-api-client"))
     implementation(libs.spring.boot.starter.web)
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
 
     testImplementation(libs.h2.database)
 }
